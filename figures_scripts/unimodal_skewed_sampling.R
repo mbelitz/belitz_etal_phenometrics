adaaeaf58bc4f21442d13f5d99778914ed760413
Michael Belitz <michaelbelitz06@gmail.com>
2020-01-07 11:31:35 -0500

    made manuscript ready figures

--- figures_scripts/unimodal_skewed_sampling.R
+++ figures_scripts/unimodal_skewed_sampling.R
@@ -14,7 +14,7 @@ uni_quantile_sims <- read.csv("results/unimodal_quantile.csv",
   dplyr::mutate(Skewed = "No")
 uni_skewed_quantile_sims <- read.csv("results/unimodal_skewed_quantile.csv",
                                      stringsAsFactors = FALSE) %>% 
-  dplyr::mutate(Estimator = 'Quantile - Skewed') %>% 
+  dplyr::mutate(Estimator = 'Quantile') %>% 
   dplyr::mutate(Skewed = "Yes")
 
 uni_phest_sims <- read.csv("results/unimodal_phest.csv", 
@@ -25,21 +25,21 @@ uni_phest_sims <- read.csv("results/unimodal_phest.csv",
 uni_skewed_phest_sims <- read.csv("results/unimodal_skewed_phest.csv",
                                   stringsAsFactors = FALSE)%>% 
   dplyr::select(-lowCI, -highCI) %>% 
-  dplyr::mutate(Estimator = 'Phest - Skewed')%>% 
+  dplyr::mutate(Estimator = 'Phest')%>% 
   dplyr::mutate(Skewed = "Yes")
 
 uni_mean_sims <- read.csv("results/unimodal_mean.csv", stringsAsFactors = FALSE) %>% 
   dplyr::mutate(Estimator = 'Mean')%>% 
   dplyr::mutate(Skewed = "No")
 uni_skewed_mean_sims <- read.csv("results/unimodal_skewed_mean.csv", stringsAsFactors = FALSE) %>% 
-  dplyr::mutate(Estimator = 'Mean - Skewed')%>% 
+  dplyr::mutate(Estimator = 'Mean')%>% 
   dplyr::mutate(Skewed = "Yes")
 
 uni_phenesse_sims <- read.csv("results/unimodal_phenesse.csv", stringsAsFactors = FALSE) %>% 
   dplyr::mutate(Estimator = "Phenesse")%>% 
   dplyr::mutate(Skewed = "No")
 uni_skewed_phenesse_sims <- read.csv("results/unimodal_skewed_phenesse.csv", stringsAsFactors = FALSE) %>% 
-  dplyr::mutate(Estimator = "Phenesse - Skewed")%>% 
+  dplyr::mutate(Estimator = "Phenesse")%>% 
   dplyr::mutate(Skewed = "Yes")
 
 
@@ -86,18 +86,43 @@ um_skewed_rmse
 ### Now only do skewed
 
 um_skewed_rmse_data <- total_skewed_uni_metrics_barplot %>% 
-  dplyr::filter(Skewed == "Yes")
+  dplyr::filter(Skewed == "Yes") %>% 
+  mutate(Estimator = factor(Estimator, levels = c('Phenesse', 
+                                                  'Quantile', 
+                                                  'Mean',
+                                                  'Phest')))
 
-um_rmse_data <- total_skewed_uni_metrics_barplot %>% 
-  dplyr::filter(Skewed == "No")
-
-ggplot() + 
+um_rmse_skewed <- ggplot() + 
   geom_bar(data = um_skewed_rmse_data, aes(x = fac_Q, y = RMSE, fill = Estimator), 
            stat = "identity", size = 1, alpha = 0.8, position = "dodge") +
   ggtitle("Unimodal Distribution") + 
   scale_y_continuous(expand = c(0,0)) +
   theme_bw() +
-  facet_grid(sd~obs, labeller = labeller()) +
+  facet_grid(sd~obs) +
   labs(x = "Percentile", y = 'RMSE') + 
-  scale_fill_viridis_d() +
-  theme(plot.title = element_text(hjust = 0.5)) 
+  scale_fill_manual(values = c("#440154FF", "#287D8EFF","#FDE725FF", "#73D055FF")) +
+  theme(plot.title = element_text(hjust = 0.5),
+        panel.spacing = unit(0.75, "lines")) 
+
+um_rmse_skewed
+
+ggplot2::ggsave("figures_outputs/skewed_um_rmse.png", plot = um_rmse_skewed, width = 10, height = 8, dpi = 300)
+
+##### Skewed UNIMODAL BIAS PLOT #########
+
+um_bias_skewed <- ggplot(um_skewed_rmse_data) + 
+  geom_bar(aes(x = fac_Q, y = Bias, fill = Estimator), 
+           stat = "identity", size = 1, alpha = 0.8, position = "dodge") +
+  geom_hline(yintercept = 0, alpha = 0.5) +
+  ggtitle("Unimodal Distribution") + 
+  scale_y_continuous(expand = c(0,0)) +
+  theme_bw() +
+  scale_fill_manual(values = c("#440154FF", "#287D8EFF","#FDE725FF", "#73D055FF")) +
+  facet_grid(sd~obs, labeller = labeller()) +
+  labs(x = "Percentile", y = 'Bias') + 
+  theme(plot.title = element_text(hjust = 0.5),
+        panel.spacing = unit(0.75, "lines")) 
+
+um_bias_skewed
+
+ggplot2::ggsave("figures_outputs/skewed_unimodal_bias.png", plot = um_bias_skewed, width = 10, height = 8, dpi = 300)
